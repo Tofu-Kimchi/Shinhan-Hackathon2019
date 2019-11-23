331fc5265b08c7beeda59fb826f647c98f505502
Tofu-Kimchi <cseunggyeongg@gmail.com>
2019-11-23 14:56:15 +0900

    move next page finish

--- app/src/main/java/com/khan/guu/a2019hackethonshinhan/MainActivity.java
+++ app/src/main/java/com/khan/guu/a2019hackethonshinhan/MainActivity.java
@@ -4,17 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView)findViewById(R.id.img_main);
+        iv.setOnClickListener(new MyListener());
     }
 
-    public void btn_main(View v){
-        Intent it = getIntent();
-        startActivity(it);
+    class MyListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Intent it = new Intent(getApplicationContext(), GaipActivity.class);
+            startActivity(it);
+        }
     }
 }
