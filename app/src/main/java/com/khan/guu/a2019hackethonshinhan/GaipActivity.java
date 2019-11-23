bd8376b25929f05017ee92f093c80925c86f8111
DESKTOP-M3IP4EM\guhwan <ghkim110>
2019-11-23 14:49:53 +0900

    수정

--- app/src/main/java/com/khan/guu/a2019hackethonshinhan/GaipActivity.java
+++ app/src/main/java/com/khan/guu/a2019hackethonshinhan/GaipActivity.java
@@ -7,8 +7,8 @@ public class GaipActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gaip);
 
-        getIntent();
+//        getIntent();
     }
 }
